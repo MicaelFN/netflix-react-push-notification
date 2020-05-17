02077d73404c093490c3ba0548b93a15b8764c08
MicaelFN <micael.f.noronha@gmail.com>
2020-05-17 10:43:34 -0300

    foreground/background

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React ,{useEffect} from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 import Home from './screen/Home'
 
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+	console.log('Message handled in the background!', remoteMessage);
+  });
+  
+
 const App = () => {
+
+	useEffect(() => {
+		const unsubscribe = messaging().onMessage(async remoteMessage => {
+		  Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+		});
+	
+		return unsubscribe;
+	  }, []);
+
 	return <Home />
 }
 
